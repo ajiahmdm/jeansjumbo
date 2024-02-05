ef5b483a9f94666c95ee2b864a29e04ff20b48f1
ajiahmdm <ajiakhmadm@gmail.com>
2024-02-05 15:19:35 +0700

    api tele

---
+++ sender.js
@@ -0,0 +1,35 @@
+//bot token
+var telegram_bot_id = "6931807566:AAFCX387aeegRpQP9nO-m9pzfrnkE0OdoIk";
+//chat id
+var chat_id = 6950739980;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
