1f93994d31e11ed130a2703a8487c118bcbb84f3
ajiahmdm <ajiakhmadm@gmail.com>
2024-02-12 05:30:54 +0700

    fix form

--- sender.js
+++ sender.js
@@ -1,13 +1,35 @@
+/*
+const form = document.querySelector('#form')
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    var text1 = document.querySelector("#text1").value
+    var text2 = document.querySelector("#text2").value
+
+    var my_text = 'Pesanan Dari:%0A - Text1: ${text1} %0A - Text2: ${text2}'
+
+    var chat_id = 6931807566
+    var url = 'https://api.telegram.org/bot6931807566:AAFCX387aeegRpQP9nO-m9pzfrnkE0OdoIk/sendMessage?chat_id=${chat_id}&text=${my_text}'
+
+    let api = new XMLHttpRequest()
+    api.open("GET", url, true)
+    api.send()
+
+    
+
+
+})
+*/
 //bot token
 var telegram_bot_id = "6931807566:AAFCX387aeegRpQP9nO-m9pzfrnkE0OdoIk";
 //chat id
-var chat_id = 6950739980;
+var chat_id = -4108530471;
 var u_name, email, message;
 var ready = function () {
     u_name = document.getElementById("name").value;
     email = document.getElementById("email").value;
-    message = document.getElementById("message").value;
-    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+    message = "Name: " + u_name + "\nNo WA: " + email;
 };
 var sender = function () {
     ready();
@@ -30,6 +52,5 @@ var sender = function () {
     });
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
     return false;
 };
